063985a6372b9aa3d9c61fd958e3ad21e484ff68
Michael Mintz <mdmintz@gmail.com>
2024-06-28 22:37:46 -0400

    Version 1.5.1

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ if sys.argv[-1] == "publish":
 
 setup(
     name="pdbp",
-    version="1.5.0",
+    version="1.5.1",
     description="pdbp (Pdb+): A drop-in replacement for pdb and pdbpp.",
     long_description=long_description,
     long_description_content_type="text/markdown",
