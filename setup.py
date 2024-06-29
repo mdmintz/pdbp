e3aea8ba9316ec8b848a99b65fbdde5ebbe03eeb
Michael Mintz <mdmintz@gmail.com>
2024-06-28 22:36:42 -0400

    Refresh Python dependencies

--- setup.py
+++ setup.py
@@ -121,7 +121,7 @@ setup(
     ],
     python_requires=">=3.7",
     install_requires=[
-        "pygments>=2.16.1",
+        "pygments>=2.17.2",
         "tabcompleter>=1.3.0",
         'colorama>=0.4.6;platform_system=="Windows"',
     ],
